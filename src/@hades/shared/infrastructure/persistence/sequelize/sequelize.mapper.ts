afd4bfc7b02fd563a4b40905de725c95695d91ea
Carlos <carlos.rodriguez@techedgegroup.com>
2020-06-16 09:32:16 +0200

    minor fix

--- src/@hades/shared/infrastructure/persistence/sequelize/sequelize.mapper.ts
+++ src/@hades/shared/infrastructure/persistence/sequelize/sequelize.mapper.ts
@@ -1,7 +1,7 @@
 import { BaseEntity } from "@hades/shared/domain/lib/base-entity";
 import { ObjectLiteral } from "@hades/shared/domain/lib/object-literal";
 
-export interface SequelizeOrmMapper
+export interface SequelizeMapper
 {
     mapToEntity(object: ObjectLiteral | ObjectLiteral[]): BaseEntity | BaseEntity[];
 }
