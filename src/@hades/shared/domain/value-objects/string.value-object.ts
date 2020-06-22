89b7b1c0fab35e3efa6b4db941f9cfa5aa1fce4d
Carlos <carlos.rodriguez@techedgegroup.com>
2020-06-22 17:40:48 +0200

    add validations to

--- src/@hades/shared/domain/value-objects/string.value-object.ts
+++ src/@hades/shared/domain/value-objects/string.value-object.ts
@@ -1,27 +1,30 @@
 import { ValueObject } from './value-object';
 import { BadRequestException } from '@nestjs/common';
+import { ValidationRules } from './../lib/validation-rules';
 
 export abstract class StringValueObject implements ValueObject<string>
 {
     public readonly type: string;
-    public readonly length: number;
-    public readonly minLength: number;
-    public readonly maxLength: number;
-
-    constructor(
-        private _value: string
-    ) {}
+    private _value: string;
+    public validationRules: ValidationRules;
+    
+    constructor(value: string, validationRules?: ValidationRules) 
+    {
+        this.validationRules = validationRules;
+        this.value = value;
+    }
 
     get value(): string
     {
         if (this._value === "") return null;
         return this._value;
     }
+    
     set value(value: string)
     {
-        if (!!this.minLength && value.length < this.minLength)  throw new BadRequestException(`Value for ${this.type} is too short, has a minimum length of ${this.maxLength}`);
-        if (!!this.maxLength && value.length > this.maxLength)  throw new BadRequestException(`Value for ${this.type} is too large, has a maximum length of ${this.maxLength}`);
-        if (!!this.length && value.length > this.length)        throw new BadRequestException(`Value for ${this.type} is not allowed, must be a length of ${this.length}`);
+        if (!!this.validationRules?.minLength && value.length < this.validationRules.minLength)  throw new BadRequestException(`Value for ${this.validationRules.name} is too short, has a minimum length of ${this.validationRules.minLength}`);
+        if (!!this.validationRules?.maxLength && value.length > this.validationRules.maxLength)  throw new BadRequestException(`Value for ${this.validationRules.name} is too large, has a maximum length of ${this.validationRules.maxLength}`);
+        if (!!this.validationRules?.length && value.length > this.validationRules.length)        throw new BadRequestException(`Value for ${this.validationRules.name} is not allowed, must be a length of ${this.validationRules.length}`);
 
         this._value = value;
     }
