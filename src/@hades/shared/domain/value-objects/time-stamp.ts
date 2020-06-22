d1e8b0d2e2749f6b6ab79a6582865bc975a65039
Carlos <carlos.rodriguez@techedgegroup.com>
2020-06-22 18:58:02 +0200

    add nullable validator

--- src/@hades/shared/domain/value-objects/time-stamp.ts
+++ src/@hades/shared/domain/value-objects/time-stamp.ts
@@ -1,12 +1,11 @@
 import { StringValueObject } from './string.value-object';
+import { ValidationRules } from './../lib/validation-rules';
 import * as moment from 'moment';
 export abstract class TimeStamp extends StringValueObject
 {
-    constructor(
-        _value?: string
-    ) 
+    constructor(_value?: string, validationRules?: ValidationRules) 
     {
-        super(_value);
+        super(_value, validationRules);
        
         this.ensureIsValidTimeStamp(_value);
     }
