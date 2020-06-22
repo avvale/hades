731dcf3399ed5ca14f4f41ec05d7cffe9786182d
Carlos <carlos.rodriguez@techedgegroup.com>
2020-06-22 18:26:56 +0200

    add validationRules

--- src/@hades/admin/lang/domain/value-objects/lang-id.ts
+++ src/@hades/admin/lang/domain/value-objects/lang-id.ts
@@ -3,4 +3,11 @@ import { Uuid } from '@hades/shared/domain/value-objects/uuid';
 export class LangId extends Uuid 
 {
     public readonly type: 'LangId';
+
+    constructor(value: string) 
+    {
+        super(value, { 
+            name: 'LangId'
+        });
+    }
 }
