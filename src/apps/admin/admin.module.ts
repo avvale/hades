6c1e3c1a86d6eab87c0648aeeb574f00ed24d012
Carlos <carlos.rodriguez@techedgegroup.com>
2020-06-24 19:59:19 +0200

    minor fix

--- src/apps/admin/admin.module.ts
+++ src/apps/admin/admin.module.ts
@@ -5,26 +5,26 @@ import { SharedModule } from './../shared/shared.module';
 import { AdminHandlers, AdminServices, AdminModels, AdminRepositories, AdminSagas } from '@hades/admin';
 
 // controllers
-import { CreateLangController } from './controllers/lang/create-lang.controller';
-import { InsertLangsController } from './controllers/lang/insert-langs.controller';
-import { PaginateLangsController } from './controllers/lang/paginate-langs.controller';
-import { GetLangsController } from './controllers/lang/get-langs.controller';
-import { FindLangByIdController } from './controllers/lang/find-lang-by-id.controller';
-import { FindLangController } from './controllers/lang/find-lang.controller';
-import { UpdateLangController } from './controllers/lang/update-lang.controller';
-import { DeleteLangByIdController } from './controllers/lang/delete-lang-by-id.controller';
-import { DeleteLangsController } from './controllers/lang/delete-langs.controller';
+import { CreateLangController } from './lang/controllers/create-lang.controller';
+import { InsertLangsController } from './lang/controllers/insert-langs.controller';
+import { PaginateLangsController } from './lang/controllers/paginate-langs.controller';
+import { GetLangsController } from './lang/controllers/get-langs.controller';
+import { FindLangByIdController } from './lang/controllers/find-lang-by-id.controller';
+import { FindLangController } from './lang/controllers/find-lang.controller';
+import { UpdateLangController } from './lang/controllers/update-lang.controller';
+import { DeleteLangByIdController } from './lang/controllers/delete-lang-by-id.controller';
+import { DeleteLangsController } from './lang/controllers/delete-langs.controller';
 
 // resolvers
-import { CreateLangResolver } from './resolvers/lang/create-lang.resolver';
-import { InsertLangsResolver } from './resolvers/lang/insert-langs.resolver';
-import { PaginateLangsResolver } from './resolvers/lang/paginate-langs.resolver';
-import { GetLangsResolver } from './resolvers/lang/get-langs.resolver';
-import { FindLangResolver } from './resolvers/lang/find-lang.resolver';
-import { FindLangByIdResolver } from './resolvers/lang/find-lang-by-id.resolver';
-import { UpdateLangResolver } from './resolvers/lang/update-lang.resolver';
-import { DeleteLangByIdResolver } from './resolvers/lang/delete-lang-by-id.resolver';
-import { DeleteLangsResolver } from './resolvers/lang/delete-langs.resolver';
+import { CreateLangResolver } from './lang/resolvers/create-lang.resolver';
+import { InsertLangsResolver } from './lang/resolvers/insert-langs.resolver';
+import { PaginateLangsResolver } from './lang/resolvers/paginate-langs.resolver';
+import { GetLangsResolver } from './lang/resolvers/get-langs.resolver';
+import { FindLangResolver } from './lang/resolvers/find-lang.resolver';
+import { FindLangByIdResolver } from './lang/resolvers/find-lang-by-id.resolver';
+import { UpdateLangResolver } from './lang/resolvers/update-lang.resolver';
+import { DeleteLangByIdResolver } from './lang/resolvers/delete-lang-by-id.resolver';
+import { DeleteLangsResolver } from './lang/resolvers/delete-langs.resolver';
 
 @Module({
     imports: [
