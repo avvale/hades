5e8a1145271fa6325a199b06441a5aefc646bc27
Carlos <carlos.rodriguez@techedgegroup.com>
2020-06-25 00:25:26 +0200

    add number value object

--- src/@hades/shared/domain/value-objects/int.value-object.ts
+++ src/@hades/shared/domain/value-objects/int.value-object.ts
@@ -1,17 +1,15 @@
-import { ValueObject } from './value-object';
+import { NumberValueObject } from './number.value-object';
 import { BadRequestException } from '@nestjs/common';
 
-export abstract class IntValueObject extends ValueObject<number>
+export abstract class IntValueObject extends NumberValueObject
 {
     get value(): number
     {
-        if (<unknown>this._value === "") return null;
         return super.value;
     }
     
     set value(value: number)
     {
-        if (value === <number><unknown>'') value = null;
         if (value !== undefined && value !== null && !Number.isInteger(value)) throw new BadRequestException(`Value for ${this.validationRules.name} has to be a integer value`);
 
         super.value = value;
