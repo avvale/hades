3d7b89c713da738682908b86fbcb5f95b8888731
your.user.name <carlos.rodriguez@techedgegroup.com>
2020-07-06 22:40:37 +0200

    minor fix

--- test/acceptance/bplus-it-sappi/flow.e2e-spec.ts
+++ test/acceptance/bplus-it-sappi/flow.e2e-spec.ts
@@ -4,6 +4,7 @@ import { SequelizeModule } from '@nestjs/sequelize';
 import { IFlowRepository } from '@hades/bplus-it-sappi/flow/domain/flow.repository';
 import { MockFlowRepository } from '@hades/bplus-it-sappi/flow/infrastructure/mock/mock-flow.repository';
 import { GraphQLConfigModule } from './../../../src/apps/core/modules/graphql/graphql-config.module';
+import { AdminModule } from './../../../src/apps/admin/admin.module';
 import { BplusItSappiModule } from './../../../src/apps/bplus-it-sappi/bplus-it-sappi.module';
 import { Command, Operator } from '@hades/shared/domain/persistence/sql-statement-input';
 import * as request from 'supertest';
@@ -18,6 +19,7 @@ describe('flow', () =>
     {
         const module: TestingModule = await Test.createTestingModule({
                 imports: [
+                    AdminModule,
                     BplusItSappiModule,
                     GraphQLConfigModule,
                     SequelizeModule.forRootAsync({
