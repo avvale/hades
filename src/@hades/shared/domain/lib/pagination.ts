80a9ed15e6e78a7c14fa58c8575e220509a0fbcd
your.user.name <carlos.rodriguez@techedgegroup.com>
2020-07-08 18:58:45 +0200

    add pagination query definition

--- src/@hades/shared/domain/lib/pagination.ts
+++ src/@hades/shared/domain/lib/pagination.ts
@@ -1,6 +1,25 @@
-export interface Pagination<Aggregate>
+import { ApiProperty } from '@nestjs/swagger';
+
+export class Pagination<Aggregate>
 {
-    total: number,
-    count: number,
-    rows: Aggregate[],
+    @ApiProperty({
+        type        : Number,
+        description : 'Total records of the query ',
+        example     : 100
+    })
+    total: number;
+
+    @ApiProperty({
+        type        : Number,
+        description : 'Total records applying constraints',
+        example     : 75
+    })
+    count: number;
+
+    @ApiProperty({
+        type        : [Object],
+        description : 'Rows obtained from the query',
+        example     : [{id: 1, name: 'John', surname: 'Doe'}, {id: 2, name: 'Mike', surname: 'Tall'}]
+    })
+    rows: Aggregate[];
 }
