06c9e35208e04f0fef9d40876e9a80dcf96bbac6
Carlos <carlos.rodriguez@techedgegroup.com>
2020-07-27 23:41:21 +0200

    add validation uuid

--- src/@hades/shared/domain/value-objects/uuid.value-object.ts
+++ src/@hades/shared/domain/value-objects/uuid.value-object.ts
@@ -1,25 +1,18 @@
 import { BadRequestException } from '@nestjs/common';
-import { v4 as uuidv4 } from 'uuid';
-import * as validate from 'uuid-validate';
 import { StringValueObject } from './string.value-object';
-import { ValidationRules } from './../lib/validation-rules';
+import * as validate from 'uuid-validate';
 
 export abstract class UuidValueObject extends StringValueObject
 {
-    constructor(value: string, validationRules: ValidationRules) 
+    get value(): string
     {
-        super(value, Object.assign(validationRules, { length: 36 }));
-        this.checkIfIsNull();
-        this.ensureIsValidUuid();
+        return super.value;
     }
-
-    private ensureIsValidUuid(): void
-    {     
+    
+    set value(value: string)
+    {
         if (!validate(this.value, 4)) throw new BadRequestException(`Value for ${this.validationRules.name} has value: ${this.value}, not allowed for uuid`);  
-    }
 
-    private checkIfIsNull(): void
-    {
-        if (!this.value) this.value = uuidv4();
+        super.value = value;
     }
 }
