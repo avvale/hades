9ddb86abc648741da1e87b7d2192c8963a17db49
Carlos <carlos.rodriguez@techedgegroup.com>
2020-07-28 18:15:50 +0200

    add logger

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,13 @@ import { NestFactory } from '@nestjs/core';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { EnvironmentService } from '@hades/shared/domain/environment/environment.service';
 import { AppModule } from './app.module';
+import { LoggerService } from './apps/core/modules/logger/logger.service';
 
 async function bootstrap() 
 {
-    const app                   = await NestFactory.create(AppModule);
+    const app                   = await NestFactory.create(AppModule, { logger: false });
     const environmentService    = app.get(EnvironmentService);
+    const loggerService         = app.get(LoggerService);
 
     // set swagger config
     const options = new DocumentBuilder()
@@ -17,6 +19,7 @@ async function bootstrap()
     const document = SwaggerModule.createDocument(app, options);
     SwaggerModule.setup('api', app, document);
     
+    app.useLogger(loggerService);
     await app.listen(environmentService.get<number>('APP_PORT'));
 }
 bootstrap();
