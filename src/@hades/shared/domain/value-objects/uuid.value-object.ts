d07b8e326cafa6b95ea15998cc9cd19366c7703c
Carlos <carlos.rodriguez@techedgegroup.com>
2020-07-29 18:57:27 +0200

    minor fix

--- src/@hades/shared/domain/value-objects/uuid.value-object.ts
+++ src/@hades/shared/domain/value-objects/uuid.value-object.ts
@@ -11,8 +11,8 @@ export abstract class UuidValueObject extends StringValueObject
     
     set value(value: string)
     {
-        // null and undefined validation checked in StringValueObject
-        if (value && !validate(value, 4)) throw new BadRequestException(`Value for ${this.validationRules.name} has value: ${this.value}, not allowed for uuid`);
+        // null, undefined and length validation checked in StringValueObject
+        if (value && value.length === 36 && !validate(value, 4)) throw new BadRequestException(`Value for ${this.validationRules.name} has value: ${value}, not allowed for uuid`);
 
         super.value = value;
     }
