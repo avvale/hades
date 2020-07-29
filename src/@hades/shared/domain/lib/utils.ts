4955ecedcb97e9fe537f1af496e6dcaa34aec858
Carlos <carlos.rodriguez@techedgegroup.com>
2020-07-30 01:56:04 +0200

    - add sha1 - add encode and decode base64

--- src/@hades/shared/domain/lib/utils.ts
+++ src/@hades/shared/domain/lib/utils.ts
@@ -1,9 +1,29 @@
 import * as moment from 'moment';
+import * as crypto from 'crypto';
+declare const Buffer;
 
 export class Utils
 {
     public static nowTimestamp(): string
     {
         return moment().format('YYYY-MM-DD h:mm:ss');
+    }
+
+    public static sha1(data: string): string
+    {
+        const generator = crypto.createHash('sha1');
+        generator.update(data);  
+        
+        return generator.digest('hex');
+    }
+   
+    public static base64Encode(data: string): string
+    {
+        return Buffer.from(data).toString('base64');
+    }
+
+    public static base64Decode(data: string): string
+    {
+        return Buffer.from(data, 'base64').toString('utf-8')
     }
 }
