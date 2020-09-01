0e9658f658e35b9e2debb346b5d331e8f3ae5f57
Carlos <carlos.rodriguez@techedgegroup.com>
2020-09-01 22:11:31 +0200

    minor fix

--- src/apps/bplus-it-sappi/snapshot/controllers/create-snapshot.controller.ts
+++ src/apps/bplus-it-sappi/snapshot/controllers/create-snapshot.controller.ts
@@ -199,7 +199,7 @@ export class CreateSnapshotController
             },
             {
                 command: Command.LIMIT,
-                value: 50000
+                value: 30000
             }
         ]));
         await this.commandBus.dispatch(new CreateMessagesDetailCommand(messagesDetail))
@@ -240,7 +240,7 @@ export class CreateSnapshotController
             },
             {
                 command: Command.LIMIT,
-                value: 50000
+                value: 30000
             }
         ]));
         await this.commandBus.dispatch(new CreateChannelsDetailCommand(channelsDetail));
@@ -281,7 +281,7 @@ export class CreateSnapshotController
             },
             {
                 command: Command.LIMIT,
-                value: 50000
+                value: 30000
             }
         ]));
         await this.commandBus.dispatch(new CreateJobsDetailCommand(jobsDetail));
