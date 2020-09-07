b8ae494df1f5bb957c2550628c34ee2ead7bfa97
Carlos <carlos.rodriguez@techedgegroup.com>
2020-09-07 23:36:11 +0200

    add admin/lang seeder

--- src/apps/admin/lang/seeder/seeder.module.ts
+++ src/apps/admin/lang/seeder/seeder.module.ts
@@ -1,9 +1,7 @@
 import { Module } from '@nestjs/common';
-
-import { AdminModule } from 'src/apps/admin/admin.module';
-
 import { SharedModule } from 'src/apps/shared/shared.module';
 import { SequelizeConfigModule } from 'src/apps/core/modules/sequelize/sequelize-config.module';
+import { AdminModule } from 'src/apps/admin/admin.module';
 
 @Module({
     imports: [
--- src/apps/admin/lang/seeder/seeder.ts
+++ src/apps/admin/lang/seeder/seeder.ts
@@ -1,14 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { ICommandBus } from '@hades/shared/domain/bus/command-bus';
-import { langs } from './lang.seed';
-import { CreateLangsCommand } from '../../application/create/create-langs.command';
+import { CreateLangsCommand } from '@hades/admin/lang/application/create/create-langs.command';
 import { SeederModule } from './seeder.module';
+import { langs } from '@hades/admin/lang/infrastructure/seeds/lang.seed';
 
 export class Seeder 
 {
     main()
     {
-        console.log('OK');
         NestFactory.createApplicationContext(SeederModule).then(appContext => {
             const commandBus = appContext.get(ICommandBus);
             commandBus.dispatch(new CreateLangsCommand(langs));
