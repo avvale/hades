7e842eef9082fa3b6c27f49c1f7d8bfde4475bf2
Carlos <carlos.rodriguez@techedgegroup.com>
2020-09-09 01:18:15 +0200

    minor fix

--- src/graphql.ts
+++ src/graphql.ts
@@ -282,10 +282,10 @@ export interface Pagination {
 }
 
 export interface OAuthCredential {
-    tokenType: string;
-    accessToken: string;
-    refreshToken?: string;
-    expiresIn?: number;
+    token_type: string;
+    access_token: string;
+    refresh_token?: string;
+    expires_in?: number;
     scope?: string;
 }
 
