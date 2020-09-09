c3fc2bc9a629f04a21abf23d8ac2feb123e938ef
Carlos <carlos.rodriguez@techedgegroup.com>
2020-09-09 11:00:05 +0200

    add findMe resolver

--- src/apps/oauth/credential/index.ts
+++ src/apps/oauth/credential/index.ts
@@ -2,10 +2,12 @@
 
 // resolvers
 import { CreateCredentialResolver } from "./resolvers/create-credential.resolver";
+import { FindMeResolver } from './resolvers/find-me.resolver';
 
 export const OAuthCredentialControllers = [
 ];
 
 export const OAuthCredentialResolvers = [
     CreateCredentialResolver,
+    FindMeResolver
 ];
