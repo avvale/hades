85a7de23633ad1710cddfd4d6befc8a141c389e8
Carlos <carlos.rodriguez@techedgegroup.com>
2020-09-09 14:42:32 +0200

    minor fix

--- src/@hades/o-auth/application/infrastructure/seeds/application.seed.ts
+++ src/@hades/o-auth/application/infrastructure/seeds/application.seed.ts
@@ -1,3 +1,3 @@
 export const applications = [
-    { uuid: '8bb03dc8-c97b-4e06-b1b0-3c62e108fd80', code: 'quasar', secret: '4$rAt@p2Y3$3', name: 'Quasar' }
+    { id: '8bb03dc8-c97b-4e06-b1b0-3c62e108fd80', code: 'quasar', secret: '4$rAt@p2Y3$3', name: 'Quasar' }
 ];
