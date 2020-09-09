6474ae6ac78ff78eec4e780d204767822350a382
Carlos <carlos.rodriguez@techedgegroup.com>
2020-09-09 14:28:30 +0200

    minor fix

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common';
 
 import { CoreModule } from './apps/core/core.module';
-import { OAuthModule } from './apps/oauth/oauth.module';
+import { OAuthModule } from './apps/o-auth/o-auth.module';
 import { AdminModule } from './apps/admin/admin.module';
 
 @Module({
