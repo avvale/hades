e268d1757626078da038913fc3dd9779a13d2585
Carlos <carlos.rodriguez@techedgegroup.com>
2020-09-11 13:55:18 +0200

    minor fix

--- src/@hades/o-auth/application/infrastructure/seeds/application.seed.ts
+++ src/@hades/o-auth/application/infrastructure/seeds/application.seed.ts
@@ -1,3 +1,3 @@
 export const applications = [
-    { id: '8bb03dc8-c97b-4e06-b1b0-3c62e108fd80', code: 'hades.admin.user', secret: '4$rAt@p2Y3$3', name: 'Hades admin user' }
+    { id: '8bb03dc8-c97b-4e06-b1b0-3c62e108fd80', code: 'hades.admin.user', secret: '4$rAt@p2Y3$3', name: 'Hades admin user', isMaster: true }
 ];
