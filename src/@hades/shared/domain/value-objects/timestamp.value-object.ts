5de4c0acd481a12a8e6d5180d9e88bea4791fbcb
Carlos <carlos.rodriguez.palacin@gmail.com>
2020-09-17 11:27:26 +0200

    avoid manage null and undefined values, return a Invalid date string in timestamp value object

--- src/@hades/shared/domain/value-objects/timestamp.value-object.ts
+++ src/@hades/shared/domain/value-objects/timestamp.value-object.ts
@@ -14,6 +14,9 @@ export abstract class TimestampValueObject extends StringValueObject
         // first pass value to super to pass validations
         super.value = value;
 
+        // avoid manage null and undefined values, return a Invalid date string
+        if (value === null || value === undefined) return;
+
         if (value !== null && value !== undefined && !((new Date(value)).getTime() > 0)) throw new BadRequestException(`Value for ${this.validationRules.name} has to be a timestamp value, value ${value} is a not valid timestamp`);
 
         if (process.env.TZ) 
