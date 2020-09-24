32c074a1669cd216c1408f0dbb38a2f54aa500fa
Carlos <carlos.rodriguez.palacin@gmail.com>
2020-09-24 17:38:16 +0200

    add FindUserByUsernamePassword

---
+++ src/@hades/iam/user/application/find/find-user-by-username-password.service.ts
@@ -0,0 +1,33 @@
+import { Injectable } from '@nestjs/common';
+import { IUserRepository } from './../../domain/user.repository';
+import { IamUser } from './../../domain/user.aggregate';
+import { UserPassword, UserUsername } from './../../domain/value-objects';
+import * as bcrypt from 'bcrypt';
+
+@Injectable()
+export class FindUserByUsernamePasswordService
+{
+    constructor(
+        private readonly repository: IUserRepository
+    ) {}
+
+    public async main(username: UserUsername, password: UserPassword): Promise<IamUser>
+    {
+        const user = await this.repository.find({
+            where: {
+                username: username.value
+            },
+            include: ['account']
+        });
+
+        if (user && bcrypt.compareSync(password, user.password)) 
+        {
+            // delete password from response
+            user.password = undefined;
+
+            return user;
+        }
+
+        return null;
+    }
+}
