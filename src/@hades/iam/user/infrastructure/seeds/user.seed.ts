c628bac64749ecc24c1d6436513a5db4c384c3ab
Carlos <carlos.rodriguez.palacin@gmail.com>
2020-09-28 12:42:37 +0200

    minor fix

--- src/@hades/iam/user/infrastructure/seeds/user.seed.ts
+++ src/@hades/iam/user/infrastructure/seeds/user.seed.ts
@@ -2,7 +2,7 @@ export const users = [
     {
         id: '23ee638e-1059-42ae-86ea-6a9df65f749a',
         accountId: '948a5308-a49d-42dc-9ea3-7490e120000b',
-        name: 'John'
+        name: 'John',
         surname: 'Doe',
         avatar: null,
         mobile: null,
