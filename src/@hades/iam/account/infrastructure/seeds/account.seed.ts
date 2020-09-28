63a1e3bb5a51e64bde1f400be7d1774a2f9d49f7
Carlos <carlos.rodriguez.palacin@gmail.com>
2020-09-28 12:40:35 +0200

    minor fix

--- src/@hades/iam/account/infrastructure/seeds/account.seed.ts
+++ src/@hades/iam/account/infrastructure/seeds/account.seed.ts
@@ -2,7 +2,7 @@ export const accounts = [
     {
         id: '948a5308-a49d-42dc-9ea3-7490e120000b',
         type: 'USER',
-        name: 'John',
+        email: 'john@gmail.com',
         isActive: true,
         clientId: '333910d9-394b-42d7-b3e0-0c7ae7a54478',
         applicationCodes: ['quasar'],
--- src/@hades/iam/user/infrastructure/seeds/user.seed.ts
+++ src/@hades/iam/user/infrastructure/seeds/user.seed.ts
@@ -2,9 +2,9 @@ export const users = [
     {
         id: '23ee638e-1059-42ae-86ea-6a9df65f749a',
         accountId: '948a5308-a49d-42dc-9ea3-7490e120000b',
+        name: 'John'
         surname: 'Doe',
         avatar: null,
-        email: 'john@gmail.com',
         mobile: null,
         langId: null,
         username: 'john@gmail.com',
