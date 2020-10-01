4b2648cfe3eb4b68327e81a6400c34dba32c478a
Carlos <carlos.rodriguez.palacin@gmail.com>
2020-10-01 14:06:59 +0200

    wip

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,6 @@
 import { Module } from '@nestjs/common';
 import { CoreModule } from './apps/core/core.module';
+import { AdminModule } from './apps/admin/admin.module';
 import { CciModule } from './apps/cci/cci.module';
 import { IamModule } from './apps/iam/iam.module';
 import { OAuthModule } from './apps/o-auth/o-auth.module';
