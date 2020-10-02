61311b0a3bfce565a629c757588c616f069ec291
Carlos <carlos.rodriguez.palacin@gmail.com>
2020-10-03 01:12:48 +0200

    manage user data

--- src/apps/iam/account/resolvers/update-account.resolver.ts
+++ src/apps/iam/account/resolvers/update-account.resolver.ts
@@ -5,6 +5,7 @@ import { IamUpdateAccountInput } from './../../../../graphql';
 import { ICommandBus } from '@hades/shared/domain/bus/command-bus';
 import { IQueryBus } from '@hades/shared/domain/bus/query-bus';
 import { UpdateAccountCommand } from '@hades/iam/account/application/update/update-account.command';
+import { UpdateUserCommand } from '@hades/iam/user/application/update/update-user.command';
 import { FindAccountByIdQuery } from '@hades/iam/account/application/find/find-account-by-id.query';
 
 @Resolver()
@@ -31,6 +32,23 @@ export class UpdateAccountResolver
             payload.tenantIds,
             
         ));
+
+        if (payload.user)
+        {
+            await this.commandBus.dispatch(new UpdateUserCommand(
+                payload.user.id,
+                payload.id,
+                payload.user.name,
+                payload.user.surname,
+                payload.user.avatar,
+                payload.user.mobile,
+                payload.user.langId,
+                payload.user.username,
+                payload.user.password,
+                payload.user.rememberToken,
+                payload.user.data,
+            ));
+        }
         
         return await this.queryBus.ask(new FindAccountByIdQuery(payload.id));
     }
