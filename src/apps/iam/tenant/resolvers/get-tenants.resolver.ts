ce6dffa785b2a56dd2c64a31b3434d6f17641be2
carlospalacin <carlos.rodriguez.palacin@gmail.com>
2020-10-07 17:05:19 +0200

    wip

--- src/apps/iam/tenant/resolvers/get-tenants.resolver.ts
+++ src/apps/iam/tenant/resolvers/get-tenants.resolver.ts
@@ -5,8 +5,14 @@ import { IQueryBus } from '@hades/shared/domain/bus/query-bus';
 import { QueryStatement } from '@hades/shared/domain/persistence/sql-statement/sql-statement';
 import { GetTenantsQuery } from '@hades/iam/tenant/application/get/get-tenants.query';
 import { IamTenant } from './../../../../graphql';
+import { UseGuards } from '@nestjs/common';
+import { AuthorizationGraphQLGuard } from 'src/apps/shared/modules/auth/guards/authorization-graphql.guard';
+import { Permissions } from './../../../shared/modules/auth/decorators/permissions.decorator';
+import { AuthGraphQLJwtGuard } from 'src/apps/shared/modules/auth/guards/auth-graphql-jwt.guard';
 
 @Resolver()
+@Permissions('iam.tenant.update')
+@UseGuards(AuthGraphQLJwtGuard, AuthorizationGraphQLGuard)
 export class GetTenantsResolver
 {
     constructor(
