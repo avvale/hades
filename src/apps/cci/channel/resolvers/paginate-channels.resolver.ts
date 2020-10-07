646b60092684a4e1a77f1446d2fb6fd5ae7c6785
carlospalacin <carlos.rodriguez.palacin@gmail.com>
2020-10-07 18:43:29 +0200

    wip

--- src/apps/cci/channel/resolvers/paginate-channels.resolver.ts
+++ src/apps/cci/channel/resolvers/paginate-channels.resolver.ts
@@ -1,4 +1,9 @@
+import { UseGuards } from '@nestjs/common';
 import { Resolver, Query, Args } from '@nestjs/graphql';
+import { AuthGraphQLJwtGuard } from './../../../shared/modules/auth/guards/auth-graphql-jwt.guard';
+import { AuthorizationGraphQLGuard } from './../../../shared/modules/auth/guards/authorization-graphql.guard';
+import { Permissions } from './../../../shared/modules/auth/decorators/permissions.decorator';
+import * as _ from 'lodash';
 
 // @hades
 import { IQueryBus } from '@hades/shared/domain/bus/query-bus';
@@ -7,6 +12,8 @@ import { QueryStatement } from '@hades/shared/domain/persistence/sql-statement/s
 import { Pagination } from './../../../../graphql';
 
 @Resolver()
+@Permissions('iam.tenant.update')
+@UseGuards(AuthGraphQLJwtGuard, AuthorizationGraphQLGuard)
 export class PaginateChannelsResolver
 {
     constructor(
@@ -16,6 +23,19 @@ export class PaginateChannelsResolver
     @Query('cciPaginateChannels')
     async main(@Args('query') queryStatement?: QueryStatement, @Args('constraint') constraint?: QueryStatement): Promise<Pagination>
     {
+        const constraint2 = _.merge(constraint, {
+            where: {
+                tenantId: 'a'
+            }
+        })
+        console.log(constraint2)
+
+        console.log(constraint)
+       /*  {
+            where: {
+                tenantId
+            }
+        } */
         return await this.queryBus.ask(new PaginateChannelsQuery(queryStatement, constraint));   
     }
 }
