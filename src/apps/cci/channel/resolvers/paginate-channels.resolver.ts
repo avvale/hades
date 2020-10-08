8674f9142c664ee719a4153a7dcec38da1f45a0d
carlospalacin <carlos.rodriguez.palacin@gmail.com>
2020-10-08 23:18:00 +0200

    wip

--- src/apps/cci/channel/resolvers/paginate-channels.resolver.ts
+++ src/apps/cci/channel/resolvers/paginate-channels.resolver.ts
@@ -1,20 +1,22 @@
 import { UseGuards } from '@nestjs/common';
 import { Resolver, Query, Args } from '@nestjs/graphql';
+import * as _ from 'lodash';
+
+// authorization
+import { Permissions } from './../../../shared/modules/auth/decorators/permissions.decorator';
 import { AuthGraphQLJwtGuard } from './../../../shared/modules/auth/guards/auth-graphql-jwt.guard';
 import { AuthorizationGraphQLGuard } from './../../../shared/modules/auth/guards/authorization-graphql.guard';
-import { Permissions } from './../../../shared/modules/auth/decorators/permissions.decorator';
-import * as _ from 'lodash';
+import { CurrentAccount } from './../../../shared/decorators/current-account.decorator';
 
 // @hades
 import { IQueryBus } from '@hades/shared/domain/bus/query-bus';
 import { PaginateChannelsQuery } from '@hades/cci/channel/application/paginate/paginate-channels.query';
 import { QueryStatement } from '@hades/shared/domain/persistence/sql-statement/sql-statement';
-import { CurrentAccount } from './../../../shared/decorators/current-account.decorator';
 import { Pagination } from './../../../../graphql';
 import { AccountResponse } from '@hades/iam/account/domain/account.response';
 
 @Resolver()
-@Permissions('iam.tenant.update')
+@Permissions('iam.tenant.get')
 @UseGuards(AuthGraphQLJwtGuard, AuthorizationGraphQLGuard)
 export class PaginateChannelsResolver
 {
@@ -25,10 +27,6 @@ export class PaginateChannelsResolver
     @Query('cciPaginateChannels')
     async main(@CurrentAccount() account: AccountResponse, @Args('query') queryStatement?: QueryStatement, @Args('constraint') constraint?: QueryStatement): Promise<Pagination>
     {
-
-        console.log(account);
-        
-
         return await this.queryBus.ask(new PaginateChannelsQuery(queryStatement, constraint));   
     }
 }
