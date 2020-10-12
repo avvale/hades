cbcda780921a3f0242ee3edecf3b71e9546add07
carlospalacin <carlos.rodriguez.palacin@gmail.com>
2020-10-12 02:38:33 +0200

    add permissions

---
+++ src/@hades/cci/shared/infrastructure/seeds/permission.seed.ts
@@ -0,0 +1,22 @@
+export const permissions = [
+
+    {id: 'ae182255-c5f2-4ad3-bb5d-3c40daa0e7eb',  name: 'cci.access',                           boundedContextId: 'd164e484-2736-40a4-9aa0-7c7c62ea8498'},
+
+    {id: 'f5999a86-4e00-4ffb-8167-4db7b779a8ed',  name: 'cci.flow.access',                      boundedContextId: 'd164e484-2736-40a4-9aa0-7c7c62ea8498'},
+    {id: 'd16068d7-28c0-46d4-975d-f944e143501f',  name: 'cci.flow.get',                         boundedContextId: 'd164e484-2736-40a4-9aa0-7c7c62ea8498'},
+    {id: '69e3180f-fc39-4693-9138-f4042824fd2c',  name: 'cci.flow.create',                      boundedContextId: 'd164e484-2736-40a4-9aa0-7c7c62ea8498'},
+    {id: '635b9b31-2255-40c2-ac60-7c50f589eade',  name: 'cci.flow.update',                      boundedContextId: 'd164e484-2736-40a4-9aa0-7c7c62ea8498'},
+    {id: 'cb2cfe60-77c0-4e95-b2e4-9ece85ca18b0',  name: 'cci.flow.delete',                      boundedContextId: 'd164e484-2736-40a4-9aa0-7c7c62ea8498'},
+
+    {id: 'e8cd5827-05f0-466e-b550-e01557770734',  name: 'cci.channel.access',                   boundedContextId: 'd164e484-2736-40a4-9aa0-7c7c62ea8498'},    
+    {id: 'fa87803f-9418-42db-b7ea-eba7ccaf1c0a',  name: 'cci.channel.get',                      boundedContextId: 'd164e484-2736-40a4-9aa0-7c7c62ea8498'},
+    {id: '1ccbb982-ca10-45f6-8f3f-f1c5a008a936',  name: 'cci.channel.create',                   boundedContextId: 'd164e484-2736-40a4-9aa0-7c7c62ea8498'},
+    {id: 'a2d7d12d-64cc-44e7-a8cb-61897bdedd36',  name: 'cci.channel.update',                   boundedContextId: 'd164e484-2736-40a4-9aa0-7c7c62ea8498'},
+    {id: '05ff8b49-c294-4d8d-82f1-4d5db2b4bdb7',  name: 'cci.channel.delete',                   boundedContextId: 'd164e484-2736-40a4-9aa0-7c7c62ea8498'},
+
+    {id: '0c53b122-6d80-4a90-af1e-a2a957054d57',  name: 'cci.module.access',                    boundedContextId: 'd164e484-2736-40a4-9aa0-7c7c62ea8498'},
+    {id: '48c706aa-48bb-4ab1-b018-8d0660eb0fac',  name: 'cci.module.get',                       boundedContextId: 'd164e484-2736-40a4-9aa0-7c7c62ea8498'},
+    {id: '92a444b2-2c51-42ed-89a1-49e5fc5048cd',  name: 'cci.module.create',                    boundedContextId: 'd164e484-2736-40a4-9aa0-7c7c62ea8498'},
+    {id: 'f2b9a070-6b08-46ef-9f69-2fba7c2de159',  name: 'cci.module.update',                    boundedContextId: 'd164e484-2736-40a4-9aa0-7c7c62ea8498'},
+    {id: 'd968f739-aae1-4c32-aa24-08dbd5eabbfe',  name: 'cci.module.delete',                    boundedContextId: 'd164e484-2736-40a4-9aa0-7c7c62ea8498'},
+];
