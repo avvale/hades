5bbbd4a26895fe51dccc55b27ea0717ce5230860
carlospalacin <carlos.rodriguez.palacin@gmail.com>
2020-10-15 14:22:32 +0200

    add seeder

---
+++ src/apps/cci/seeder/seeder.ts
@@ -0,0 +1,58 @@
+import { NestFactory } from '@nestjs/core';
+import { ICommandBus } from '@hades/shared/domain/bus/command-bus';
+import { IQueryBus } from '@hades/shared/domain/bus/query-bus';
+import { SeederModule } from './seeder.module';
+import { CreateBoundedContextsCommand } from '@hades/iam/bounded-context/application/create/create-bounded-contexts.command';
+import { CreatePermissionsCommand } from '@hades/iam/permission/application/create/create-permissions.command';
+import { CreatePermissionsRolesCommand } from '@hades/iam/permission/application/create/create-permissions-roles.command';
+import { UpdateAccountCommand } from '@hades/iam/account/application/update/update-account.command';
+import { FindAccountByIdQuery } from '@hades/iam/account/application/find/find-account-by-id.query';
+
+// sources
+import { boundedContexts } from '@hades/cci/shared/infrastructure/seeds/bounded-context.seed';
+import { permissions } from '@hades/cci/shared/infrastructure/seeds/permission.seed';
+import { IamUtils } from '@hades/iam/shared/domain/lib/iam-utils';
+
+export class Seeder 
+{
+    main()
+    {
+        NestFactory.createApplicationContext(SeederModule).then(async appContext => {
+            const commandBus    = appContext.get(ICommandBus);
+            const queryBus      = appContext.get(IQueryBus);
+
+            IamUtils.iamCommonSeed(commandBus, queryBus, boundedContexts, permissions)
+
+            await this.commonActions(commandBus, queryBus);
+        }); 
+    }
+
+    async commonActions(commandBus:ICommandBus, queryBus:IQueryBus)
+    {
+        await queryBus.ask(new FindAccountByIdQuery('948a5308-a49d-42dc-9ea3-7490e120000b'));
+        await commandBus.dispatch(new CreateBoundedContextsCommand(boundedContexts));
+        await commandBus.dispatch(new CreatePermissionsCommand(permissions));
+
+        // set all permissions to administration role
+        const permissionsRoles = permissions.map(permission => {
+            return {
+                permissionId: permission.id,
+                roleId: '99b06044-fff5-4267-9314-4bae9f909010'
+            }
+        });
+        await commandBus.dispatch(new CreatePermissionsRolesCommand(permissionsRoles));
+
+        // set all permissions to administration account
+        const account = await queryBus.ask(new FindAccountByIdQuery('948a5308-a49d-42dc-9ea3-7490e120000b'));
+        await commandBus.dispatch(new UpdateAccountCommand(
+            '948a5308-a49d-42dc-9ea3-7490e120000b', // administrator account
+            undefined,
+            undefined,
+            undefined,
+            undefined,
+            undefined,
+            account.permissions.concat(permissions)
+        ));
+    }
+}
+new Seeder().main();
