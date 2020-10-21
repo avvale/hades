dc5f8ea72a4698691700b2b2297c62806c52053b
carlospalacin <carlos.rodriguez.palacin@gmail.com>
2020-10-21 12:43:53 +0200

    wip

---
+++ src/apps/cci/summary/index.ts
@@ -0,0 +1,8 @@
+// resolvers
+import { FindSystemSummaryResolver } from './resolvers/find-system-summary.resolver';
+
+export const CciSummaryControllers = [];
+
+export const CciSummaryResolvers = [
+    FindSystemSummaryResolver
+];
