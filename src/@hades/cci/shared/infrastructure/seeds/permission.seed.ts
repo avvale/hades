e1221d012dd043108a6de9d81c7efa8648b590a2
carlospalacin <carlos.rodriguez.palacin@gmail.com>
2020-10-23 10:43:26 +0200

    add permissions

--- src/@hades/cci/shared/infrastructure/seeds/permission.seed.ts
+++ src/@hades/cci/shared/infrastructure/seeds/permission.seed.ts
@@ -2,47 +2,11 @@ export const permissions = [
 
     {id: 'ae182255-c5f2-4ad3-bb5d-3c40daa0e7eb',  name: 'cci.access',                           boundedContextId: 'd164e484-2736-40a4-9aa0-7c7c62ea8498'},
 
-    {id: 'f5999a86-4e00-4ffb-8167-4db7b779a8ed',  name: 'cci.flow.access',                      boundedContextId: 'd164e484-2736-40a4-9aa0-7c7c62ea8498'},
-    {id: 'd16068d7-28c0-46d4-975d-f944e143501f',  name: 'cci.flow.get',                         boundedContextId: 'd164e484-2736-40a4-9aa0-7c7c62ea8498'},
-    {id: '69e3180f-fc39-4693-9138-f4042824fd2c',  name: 'cci.flow.create',                      boundedContextId: 'd164e484-2736-40a4-9aa0-7c7c62ea8498'},
-    {id: '635b9b31-2255-40c2-ac60-7c50f589eade',  name: 'cci.flow.update',                      boundedContextId: 'd164e484-2736-40a4-9aa0-7c7c62ea8498'},
-    {id: 'cb2cfe60-77c0-4e95-b2e4-9ece85ca18b0',  name: 'cci.flow.delete',                      boundedContextId: 'd164e484-2736-40a4-9aa0-7c7c62ea8498'},
-
-    {id: 'e8cd5827-05f0-466e-b550-e01557770734',  name: 'cci.channel.access',                   boundedContextId: 'd164e484-2736-40a4-9aa0-7c7c62ea8498'},    
+    {id: 'e8cd5827-05f0-466e-b550-e01557770734',  name: 'cci.channel.access',                   boundedContextId: 'd164e484-2736-40a4-9aa0-7c7c62ea8498'},
     {id: 'fa87803f-9418-42db-b7ea-eba7ccaf1c0a',  name: 'cci.channel.get',                      boundedContextId: 'd164e484-2736-40a4-9aa0-7c7c62ea8498'},
     {id: '1ccbb982-ca10-45f6-8f3f-f1c5a008a936',  name: 'cci.channel.create',                   boundedContextId: 'd164e484-2736-40a4-9aa0-7c7c62ea8498'},
     {id: 'a2d7d12d-64cc-44e7-a8cb-61897bdedd36',  name: 'cci.channel.update',                   boundedContextId: 'd164e484-2736-40a4-9aa0-7c7c62ea8498'},
     {id: '05ff8b49-c294-4d8d-82f1-4d5db2b4bdb7',  name: 'cci.channel.delete',                   boundedContextId: 'd164e484-2736-40a4-9aa0-7c7c62ea8498'},
-
-    {id: '0c53b122-6d80-4a90-af1e-a2a957054d57',  name: 'cci.module.access',                    boundedContextId: 'd164e484-2736-40a4-9aa0-7c7c62ea8498'},
-    {id: '48c706aa-48bb-4ab1-b018-8d0660eb0fac',  name: 'cci.module.get',                       boundedContextId: 'd164e484-2736-40a4-9aa0-7c7c62ea8498'},
-    {id: '92a444b2-2c51-42ed-89a1-49e5fc5048cd',  name: 'cci.module.create',                    boundedContextId: 'd164e484-2736-40a4-9aa0-7c7c62ea8498'},
-    {id: 'f2b9a070-6b08-46ef-9f69-2fba7c2de159',  name: 'cci.module.update',                    boundedContextId: 'd164e484-2736-40a4-9aa0-7c7c62ea8498'},
-    {id: 'd968f739-aae1-4c32-aa24-08dbd5eabbfe',  name: 'cci.module.delete',                    boundedContextId: 'd164e484-2736-40a4-9aa0-7c7c62ea8498'},
-
-    {id: 'c758d333-ab97-4b76-bdcb-1f129a85217c',  name: 'cci.execution.access',                 boundedContextId: 'd164e484-2736-40a4-9aa0-7c7c62ea8498'},
-    {id: '904c650d-ea8e-489c-9fe5-7f482862620b',  name: 'cci.execution.get',                    boundedContextId: 'd164e484-2736-40a4-9aa0-7c7c62ea8498'},
-    {id: '4fa3d4b5-9a63-4bd7-88c1-6b09765f7eb8',  name: 'cci.execution.create',                 boundedContextId: 'd164e484-2736-40a4-9aa0-7c7c62ea8498'},
-    {id: 'd67dbf60-15c2-42a7-8f4f-f13dbbb8b18d',  name: 'cci.execution.update',                 boundedContextId: 'd164e484-2736-40a4-9aa0-7c7c62ea8498'},
-    {id: '2b682c0c-ae83-4d0a-bc0b-aa316097832e',  name: 'cci.execution.delete',                 boundedContextId: 'd164e484-2736-40a4-9aa0-7c7c62ea8498'},
-
-    {id: 'b4898afe-882c-4632-bbba-945550fff2ba',  name: 'cci.dataLake.access',                  boundedContextId: 'd164e484-2736-40a4-9aa0-7c7c62ea8498'},
-    {id: 'e6b51ddc-e317-4914-ad4c-725bc97f1f34',  name: 'cci.dataLake.get',                     boundedContextId: 'd164e484-2736-40a4-9aa0-7c7c62ea8498'},
-    {id: 'ecbdbbe0-b796-4b54-8856-e26bd90dfec0',  name: 'cci.dataLake.create',                  boundedContextId: 'd164e484-2736-40a4-9aa0-7c7c62ea8498'},
-    {id: 'b9c29bd7-743a-457b-978b-e0426a05ee33',  name: 'cci.dataLake.update',                  boundedContextId: 'd164e484-2736-40a4-9aa0-7c7c62ea8498'},
-    {id: '9603c3cd-f07e-4d76-bfc0-a4b78ea39981',  name: 'cci.dataLake.delete',                  boundedContextId: 'd164e484-2736-40a4-9aa0-7c7c62ea8498'},
-
-    {id: '002e1dde-5491-4133-950d-77c000392627',  name: 'cci.jobDetail.access',                 boundedContextId: 'd164e484-2736-40a4-9aa0-7c7c62ea8498'},
-    {id: 'f8c8c75f-85d0-431b-b4bd-ab9daf7e5008',  name: 'cci.jobDetail.get',                    boundedContextId: 'd164e484-2736-40a4-9aa0-7c7c62ea8498'},
-    {id: 'e898af24-8bf6-4311-b403-233faba09651',  name: 'cci.jobDetail.create',                 boundedContextId: 'd164e484-2736-40a4-9aa0-7c7c62ea8498'},
-    {id: 'ff124d0f-a5bb-4f83-8393-e4618bab671f',  name: 'cci.jobDetail.update',                 boundedContextId: 'd164e484-2736-40a4-9aa0-7c7c62ea8498'},
-    {id: 'da3e1d7d-f7f0-44c4-86a4-ba1bc61efa76',  name: 'cci.jobDetail.delete',                 boundedContextId: 'd164e484-2736-40a4-9aa0-7c7c62ea8498'},
-
-    {id: '7de5e682-15b5-4b35-97cb-cc4ee98c05d3',  name: 'cci.jobOverview.access',               boundedContextId: 'd164e484-2736-40a4-9aa0-7c7c62ea8498'},
-    {id: 'b3b51a81-ff0a-430c-aea6-8712f1d9bc41',  name: 'cci.jobOverview.get',                  boundedContextId: 'd164e484-2736-40a4-9aa0-7c7c62ea8498'},
-    {id: '85ce6da8-6bba-4966-ba20-28a725ad0b6c',  name: 'cci.jobOverview.create',               boundedContextId: 'd164e484-2736-40a4-9aa0-7c7c62ea8498'},
-    {id: '65976c6f-0bf9-4831-bd3a-e09b7a5d688d',  name: 'cci.jobOverview.update',               boundedContextId: 'd164e484-2736-40a4-9aa0-7c7c62ea8498'},
-    {id: '9c5b28c3-d334-434f-a95c-4b0f1c8fd703',  name: 'cci.jobOverview.delete',               boundedContextId: 'd164e484-2736-40a4-9aa0-7c7c62ea8498'},
 
     {id: '39fc8166-43fa-4fa8-be11-21bf69e219d4',  name: 'cci.channelDetail.access',             boundedContextId: 'd164e484-2736-40a4-9aa0-7c7c62ea8498'},
     {id: '0d816497-2aea-46fe-a855-6466ae79d7db',  name: 'cci.channelDetail.get',                boundedContextId: 'd164e484-2736-40a4-9aa0-7c7c62ea8498'},
@@ -56,6 +20,48 @@ export const permissions = [
     {id: 'bf00e5e0-13fd-4a4d-ba4e-d073afb34a1e',  name: 'cci.channelOverview.update',           boundedContextId: 'd164e484-2736-40a4-9aa0-7c7c62ea8498'},
     {id: '672e20ae-56ad-4944-91dc-864b06ea9948',  name: 'cci.channelOverview.delete',           boundedContextId: 'd164e484-2736-40a4-9aa0-7c7c62ea8498'},
 
+    {id: '45247436-4ded-416b-b7ae-01c51c1e4179',  name: 'cci.contact.access',                   boundedContextId: 'd164e484-2736-40a4-9aa0-7c7c62ea8498'},
+    {id: '172138e6-ddce-443b-8e22-25074a8de9e4',  name: 'cci.contact.get',                      boundedContextId: 'd164e484-2736-40a4-9aa0-7c7c62ea8498'},
+    {id: '97e8c357-0a1f-453b-97df-7526da88fdac',  name: 'cci.contact.create',                   boundedContextId: 'd164e484-2736-40a4-9aa0-7c7c62ea8498'},
+    {id: '88e36c7d-aeda-424c-8d8f-842f24cffed5',  name: 'cci.contact.update',                   boundedContextId: 'd164e484-2736-40a4-9aa0-7c7c62ea8498'},
+    {id: 'b12ae240-3660-4562-8f2f-0b086c70c440',  name: 'cci.contact.delete',                   boundedContextId: 'd164e484-2736-40a4-9aa0-7c7c62ea8498'},
+
+    {id: 'a54cfe59-e12d-4cb3-8a8e-4ae7a15cc9ee',  name: 'cci.dashboard.access',                 boundedContextId: 'd164e484-2736-40a4-9aa0-7c7c62ea8498'},
+    {id: '70856476-2205-4684-9201-64d856b348d9',  name: 'cci.dashboard.get',                    boundedContextId: 'd164e484-2736-40a4-9aa0-7c7c62ea8498'},
+
+    {id: 'b4898afe-882c-4632-bbba-945550fff2ba',  name: 'cci.dataLake.access',                  boundedContextId: 'd164e484-2736-40a4-9aa0-7c7c62ea8498'},
+    {id: 'e6b51ddc-e317-4914-ad4c-725bc97f1f34',  name: 'cci.dataLake.get',                     boundedContextId: 'd164e484-2736-40a4-9aa0-7c7c62ea8498'},
+    {id: 'ecbdbbe0-b796-4b54-8856-e26bd90dfec0',  name: 'cci.dataLake.create',                  boundedContextId: 'd164e484-2736-40a4-9aa0-7c7c62ea8498'},
+    {id: 'b9c29bd7-743a-457b-978b-e0426a05ee33',  name: 'cci.dataLake.update',                  boundedContextId: 'd164e484-2736-40a4-9aa0-7c7c62ea8498'},
+    {id: '9603c3cd-f07e-4d76-bfc0-a4b78ea39981',  name: 'cci.dataLake.delete',                  boundedContextId: 'd164e484-2736-40a4-9aa0-7c7c62ea8498'},
+
+    {id: 'c758d333-ab97-4b76-bdcb-1f129a85217c',  name: 'cci.execution.access',                 boundedContextId: 'd164e484-2736-40a4-9aa0-7c7c62ea8498'},
+    {id: '904c650d-ea8e-489c-9fe5-7f482862620b',  name: 'cci.execution.get',                    boundedContextId: 'd164e484-2736-40a4-9aa0-7c7c62ea8498'},
+    {id: '4fa3d4b5-9a63-4bd7-88c1-6b09765f7eb8',  name: 'cci.execution.create',                 boundedContextId: 'd164e484-2736-40a4-9aa0-7c7c62ea8498'},
+    {id: 'd67dbf60-15c2-42a7-8f4f-f13dbbb8b18d',  name: 'cci.execution.update',                 boundedContextId: 'd164e484-2736-40a4-9aa0-7c7c62ea8498'},
+    {id: '2b682c0c-ae83-4d0a-bc0b-aa316097832e',  name: 'cci.execution.delete',                 boundedContextId: 'd164e484-2736-40a4-9aa0-7c7c62ea8498'},
+
+    {id: 'f5999a86-4e00-4ffb-8167-4db7b779a8ed',  name: 'cci.flow.access',                      boundedContextId: 'd164e484-2736-40a4-9aa0-7c7c62ea8498'},
+    {id: 'd16068d7-28c0-46d4-975d-f944e143501f',  name: 'cci.flow.get',                         boundedContextId: 'd164e484-2736-40a4-9aa0-7c7c62ea8498'},
+    {id: '69e3180f-fc39-4693-9138-f4042824fd2c',  name: 'cci.flow.create',                      boundedContextId: 'd164e484-2736-40a4-9aa0-7c7c62ea8498'},
+    {id: '635b9b31-2255-40c2-ac60-7c50f589eade',  name: 'cci.flow.update',                      boundedContextId: 'd164e484-2736-40a4-9aa0-7c7c62ea8498'},
+    {id: 'cb2cfe60-77c0-4e95-b2e4-9ece85ca18b0',  name: 'cci.flow.delete',                      boundedContextId: 'd164e484-2736-40a4-9aa0-7c7c62ea8498'},
+
+    {id: '9da3b558-ffb7-4216-b61c-bcb568018393',  name: 'cci.historic.access',                  boundedContextId: 'd164e484-2736-40a4-9aa0-7c7c62ea8498'},
+    {id: 'f67bd3db-902b-4fbb-8809-e0a8e9d75e19',  name: 'cci.historic.get',                     boundedContextId: 'd164e484-2736-40a4-9aa0-7c7c62ea8498'},
+
+    {id: '002e1dde-5491-4133-950d-77c000392627',  name: 'cci.jobDetail.access',                 boundedContextId: 'd164e484-2736-40a4-9aa0-7c7c62ea8498'},
+    {id: 'f8c8c75f-85d0-431b-b4bd-ab9daf7e5008',  name: 'cci.jobDetail.get',                    boundedContextId: 'd164e484-2736-40a4-9aa0-7c7c62ea8498'},
+    {id: 'e898af24-8bf6-4311-b403-233faba09651',  name: 'cci.jobDetail.create',                 boundedContextId: 'd164e484-2736-40a4-9aa0-7c7c62ea8498'},
+    {id: 'ff124d0f-a5bb-4f83-8393-e4618bab671f',  name: 'cci.jobDetail.update',                 boundedContextId: 'd164e484-2736-40a4-9aa0-7c7c62ea8498'},
+    {id: 'da3e1d7d-f7f0-44c4-86a4-ba1bc61efa76',  name: 'cci.jobDetail.delete',                 boundedContextId: 'd164e484-2736-40a4-9aa0-7c7c62ea8498'},
+
+    {id: '7de5e682-15b5-4b35-97cb-cc4ee98c05d3',  name: 'cci.jobOverview.access',               boundedContextId: 'd164e484-2736-40a4-9aa0-7c7c62ea8498'},
+    {id: 'b3b51a81-ff0a-430c-aea6-8712f1d9bc41',  name: 'cci.jobOverview.get',                  boundedContextId: 'd164e484-2736-40a4-9aa0-7c7c62ea8498'},
+    {id: '85ce6da8-6bba-4966-ba20-28a725ad0b6c',  name: 'cci.jobOverview.create',               boundedContextId: 'd164e484-2736-40a4-9aa0-7c7c62ea8498'},
+    {id: '65976c6f-0bf9-4831-bd3a-e09b7a5d688d',  name: 'cci.jobOverview.update',               boundedContextId: 'd164e484-2736-40a4-9aa0-7c7c62ea8498'},
+    {id: '9c5b28c3-d334-434f-a95c-4b0f1c8fd703',  name: 'cci.jobOverview.delete',               boundedContextId: 'd164e484-2736-40a4-9aa0-7c7c62ea8498'},
+
     {id: 'd3365bcc-d04b-4eb6-9042-520ba35de6a6',  name: 'cci.messageDetail.access',             boundedContextId: 'd164e484-2736-40a4-9aa0-7c7c62ea8498'},
     {id: '74ce900d-e181-4ec2-8b00-f87ef8073771',  name: 'cci.messageDetail.get',                boundedContextId: 'd164e484-2736-40a4-9aa0-7c7c62ea8498'},
     {id: 'c6e9e3cd-c268-49bb-8c4d-a1ac2f1cc92a',  name: 'cci.messageDetail.create',             boundedContextId: 'd164e484-2736-40a4-9aa0-7c7c62ea8498'},
@@ -68,14 +74,17 @@ export const permissions = [
     {id: 'e433ed37-8500-48ac-bd66-b8962c283b99',  name: 'cci.messageOverview.update',           boundedContextId: 'd164e484-2736-40a4-9aa0-7c7c62ea8498'},
     {id: '43284f8d-c8cd-41e0-a882-cb906fa7594a',  name: 'cci.messageOverview.delete',           boundedContextId: 'd164e484-2736-40a4-9aa0-7c7c62ea8498'},
 
-    {id: 'a54cfe59-e12d-4cb3-8a8e-4ae7a15cc9ee',  name: 'cci.dashboard.access',                 boundedContextId: 'd164e484-2736-40a4-9aa0-7c7c62ea8498'},
-    {id: '70856476-2205-4684-9201-64d856b348d9',  name: 'cci.dashboard.get',                    boundedContextId: 'd164e484-2736-40a4-9aa0-7c7c62ea8498'},
+    {id: '0c53b122-6d80-4a90-af1e-a2a957054d57',  name: 'cci.module.access',                    boundedContextId: 'd164e484-2736-40a4-9aa0-7c7c62ea8498'},
+    {id: '48c706aa-48bb-4ab1-b018-8d0660eb0fac',  name: 'cci.module.get',                       boundedContextId: 'd164e484-2736-40a4-9aa0-7c7c62ea8498'},
+    {id: '92a444b2-2c51-42ed-89a1-49e5fc5048cd',  name: 'cci.module.create',                    boundedContextId: 'd164e484-2736-40a4-9aa0-7c7c62ea8498'},
+    {id: 'f2b9a070-6b08-46ef-9f69-2fba7c2de159',  name: 'cci.module.update',                    boundedContextId: 'd164e484-2736-40a4-9aa0-7c7c62ea8498'},
+    {id: 'd968f739-aae1-4c32-aa24-08dbd5eabbfe',  name: 'cci.module.delete',                    boundedContextId: 'd164e484-2736-40a4-9aa0-7c7c62ea8498'},
 
-    {id: 'e62971af-7ba1-4182-afa3-e32ad3e0d26b',  name: 'cci.summary.access',                   boundedContextId: 'd164e484-2736-40a4-9aa0-7c7c62ea8498'},
-    {id: '14380c06-f27e-4d2a-bec0-8f613903a091',  name: 'cci.summary.get',                      boundedContextId: 'd164e484-2736-40a4-9aa0-7c7c62ea8498'},
-
-    {id: '9da3b558-ffb7-4216-b61c-bcb568018393',  name: 'cci.historic.access',                  boundedContextId: 'd164e484-2736-40a4-9aa0-7c7c62ea8498'},
-    {id: 'f67bd3db-902b-4fbb-8809-e0a8e9d75e19',  name: 'cci.historic.get',                     boundedContextId: 'd164e484-2736-40a4-9aa0-7c7c62ea8498'},
+    {id: '58da3f64-6585-443d-893d-e0f3e3ff8e15',  name: 'cci.role.access',                      boundedContextId: 'd164e484-2736-40a4-9aa0-7c7c62ea8498'},
+    {id: '082f6d49-7922-41a4-bff8-07752470ac65',  name: 'cci.role.get',                         boundedContextId: 'd164e484-2736-40a4-9aa0-7c7c62ea8498'},
+    {id: 'c550fba6-9ee0-4e0a-bc52-4ed689632904',  name: 'cci.role.create',                      boundedContextId: 'd164e484-2736-40a4-9aa0-7c7c62ea8498'},
+    {id: '8f158a44-d14a-40c5-a166-a635c3f5e7d7',  name: 'cci.role.update',                      boundedContextId: 'd164e484-2736-40a4-9aa0-7c7c62ea8498'},
+    {id: 'ef84b6a5-08b7-4abb-91f7-6493ee233ced',  name: 'cci.role.delete',                      boundedContextId: 'd164e484-2736-40a4-9aa0-7c7c62ea8498'},
 
     {id: 'c5cfd513-f260-42f0-b865-56312b28ba8e',  name: 'cci.rule.access',                      boundedContextId: 'd164e484-2736-40a4-9aa0-7c7c62ea8498'},
     {id: '8efb42c6-d1f5-4181-8589-e588788ad3e9',  name: 'cci.rule.get',                         boundedContextId: 'd164e484-2736-40a4-9aa0-7c7c62ea8498'},
@@ -83,15 +92,18 @@ export const permissions = [
     {id: '5e3ed8d3-43b9-41e9-9076-1e374519e9cc',  name: 'cci.rule.update',                      boundedContextId: 'd164e484-2736-40a4-9aa0-7c7c62ea8498'},
     {id: 'caeb87ca-7acc-453a-8ff7-e06355be0e22',  name: 'cci.rule.delete',                      boundedContextId: 'd164e484-2736-40a4-9aa0-7c7c62ea8498'},
 
-    {id: 'efaa311e-eebc-45ad-9133-55e3a1a9b580',  name: 'cci.task.access',                      boundedContextId: 'd164e484-2736-40a4-9aa0-7c7c62ea8498'},
-    {id: '4dfdd24e-31c1-4a6b-b75b-73bad70880b0',  name: 'cci.task.get',                         boundedContextId: 'd164e484-2736-40a4-9aa0-7c7c62ea8498'},
-    {id: '493fe054-27e3-4c46-8b95-6067231b64c5',  name: 'cci.task.create',                      boundedContextId: 'd164e484-2736-40a4-9aa0-7c7c62ea8498'},
-    {id: '6c3ffa20-a347-480c-a66d-b22395264652',  name: 'cci.task.update',                      boundedContextId: 'd164e484-2736-40a4-9aa0-7c7c62ea8498'},
-    {id: 'f93b93f8-042a-455a-9d55-36e624b3cfd3',  name: 'cci.task.delete',                      boundedContextId: 'd164e484-2736-40a4-9aa0-7c7c62ea8498'},
+    {id: 'e62971af-7ba1-4182-afa3-e32ad3e0d26b',  name: 'cci.summary.access',                   boundedContextId: 'd164e484-2736-40a4-9aa0-7c7c62ea8498'},
+    {id: '14380c06-f27e-4d2a-bec0-8f613903a091',  name: 'cci.summary.get',                      boundedContextId: 'd164e484-2736-40a4-9aa0-7c7c62ea8498'},
 
     {id: '590e7f6e-76f8-4248-82d1-86282459b081',  name: 'cci.system.access',                    boundedContextId: 'd164e484-2736-40a4-9aa0-7c7c62ea8498'},
     {id: 'c1e8d2ca-11a0-4ed7-9dfc-8b51f6351425',  name: 'cci.system.get',                       boundedContextId: 'd164e484-2736-40a4-9aa0-7c7c62ea8498'},
     {id: '0c884429-fbb8-4e40-9a93-00a9481b1aca',  name: 'cci.system.create',                    boundedContextId: 'd164e484-2736-40a4-9aa0-7c7c62ea8498'},
     {id: '5d44fd21-6c1c-4554-8452-2b4131eecb36',  name: 'cci.system.update',                    boundedContextId: 'd164e484-2736-40a4-9aa0-7c7c62ea8498'},
     {id: 'a6073251-27fe-460b-a61d-20858e91f332',  name: 'cci.system.delete',                    boundedContextId: 'd164e484-2736-40a4-9aa0-7c7c62ea8498'},
+
+    {id: 'efaa311e-eebc-45ad-9133-55e3a1a9b580',  name: 'cci.task.access',                      boundedContextId: 'd164e484-2736-40a4-9aa0-7c7c62ea8498'},
+    {id: '4dfdd24e-31c1-4a6b-b75b-73bad70880b0',  name: 'cci.task.get',                         boundedContextId: 'd164e484-2736-40a4-9aa0-7c7c62ea8498'},
+    {id: '493fe054-27e3-4c46-8b95-6067231b64c5',  name: 'cci.task.create',                      boundedContextId: 'd164e484-2736-40a4-9aa0-7c7c62ea8498'},
+    {id: '6c3ffa20-a347-480c-a66d-b22395264652',  name: 'cci.task.update',                      boundedContextId: 'd164e484-2736-40a4-9aa0-7c7c62ea8498'},
+    {id: 'f93b93f8-042a-455a-9d55-36e624b3cfd3',  name: 'cci.task.delete',                      boundedContextId: 'd164e484-2736-40a4-9aa0-7c7c62ea8498'},
 ];
