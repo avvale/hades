2e56c2e6e3e1f37897735f14bca476e06268b36a
carlospalacin <carlos.rodriguez.palacin@gmail.com>
2020-11-09 16:43:13 +0100

    wip

--- src/apps/origen/origen.module.ts
+++ src/apps/origen/origen.module.ts
@@ -1,10 +1,14 @@
 import { Module } from '@nestjs/common';
 import { SharedModule } from './../shared/shared.module';
+import { SsrControllers } from './ssr';
 
 @Module({
     imports: [
         SharedModule,
         
-    ]
+    ],
+    controllers: [
+        ...SsrControllers,
+    ],
 })
 export class OrigenModule {}
