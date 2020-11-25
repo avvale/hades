12904c33e30683b912673b6a1778fd194c0a9280
carlospalacin <carlos.rodriguez.palacin@gmail.com>
2020-11-25 19:24:02 +0100

    wip

--- src/apps/admin/administrative-area-level-1/seeder/seeder.ts
+++ src/apps/admin/administrative-area-level-1/seeder/seeder.ts
@@ -2,7 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { ICommandBus } from '@hades/shared/domain/bus/command-bus';
 import { CreateAdministrativeAreasLevel1Command } from '@hades/admin/administrative-area-level-1/application/create/create-administrative-areas-level-1.command';
 import { SeederModule } from './seeder.module';
-import { administrativeAreasLevel101, administrativeAreasLevel102, administrativeAreasLevel103, administrativeAreasLevel104, administrativeAreasLevel105 } from '@hades/admin/administrative-area-level-1/infrastructure/seeds/administrative-area-level-1.seed';
+import { administrativeAreasLevel1, administrativeAreasLevel101, administrativeAreasLevel102, administrativeAreasLevel103, administrativeAreasLevel104 } from '@hades/admin/administrative-area-level-1/infrastructure/seeds/administrative-area-level-1.seed';
 
 export class Seeder
 {
@@ -12,11 +12,11 @@ export class Seeder
             const commandBus = appContext.get(ICommandBus);
 
             await (() => {
+                commandBus.dispatch(new CreateAdministrativeAreasLevel1Command(administrativeAreasLevel1));
                 commandBus.dispatch(new CreateAdministrativeAreasLevel1Command(administrativeAreasLevel101));
                 commandBus.dispatch(new CreateAdministrativeAreasLevel1Command(administrativeAreasLevel102));
                 commandBus.dispatch(new CreateAdministrativeAreasLevel1Command(administrativeAreasLevel103));
                 commandBus.dispatch(new CreateAdministrativeAreasLevel1Command(administrativeAreasLevel104));
-                commandBus.dispatch(new CreateAdministrativeAreasLevel1Command(administrativeAreasLevel105));
             })();
         });
     }
