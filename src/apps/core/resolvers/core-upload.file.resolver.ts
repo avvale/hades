8a011703dbe21068d898e21a15dd7375824ec8e8
carlospalacin <carlos.rodriguez.palacin@gmail.com>
2020-12-15 20:12:24 +0100

    wip upload files by graphql

--- src/apps/core/resolvers/core-upload.file.resolver.ts
+++ src/apps/core/resolvers/core-upload.file.resolver.ts
@@ -1,14 +1,15 @@
 import * as fs from 'fs';
-import * as path from 'path';
 import * as fileType from 'file-type';
 import { Resolver, Args, Mutation } from '@nestjs/graphql';
-import { Upload } from 'src/graphql';
+import { EnvironmentService } from '@hades/shared/domain/services/environment.service';
 import { Utils } from '@hades/shared/domain/lib/utils';
-
+import { Upload } from 'src/graphql';
 @Resolver()
 export class CoreUploadFileResolver
 {
-    constructor() {}
+    constructor(
+        private environmentService: EnvironmentService
+    ) {}
 
     @Mutation('coreUploadFile')
     async main(@Args('path') relativeStoragePath: string, @Args('file') file: Upload)
@@ -21,19 +22,26 @@ export class CoreUploadFileResolver
         const fileTypeData      = await fileType.fromStream(readableStream);
         const hashName          = Utils.randomString(40, 'a#') + '.' + fileTypeData.ext;
         const absolutePath      = Utils.basePath('public/storage', relativeStoragePath, hashName);
-        const size              = 0; // TODO encontrar forma de conocer tamaño total
-        
-        readableStream.pipe(fs.createWriteStream(absolutePath));
+
+        // user IIFE to invoke await function with promise for upload file
+        await ((): Promise<void> => {
+            return new Promise((resolve, reject) => {
+                const writeableString = fs.createWriteStream(absolutePath);
+                writeableString.on('finish', resolve)
+                readableStream.pipe(writeableString);
+            });
+        })();
+
+        const stats = fs.statSync(absolutePath);
 
         return {
             name: filename,
             filename: hashName,
             pathname: Utils.basePath('public/storage', relativeStoragePath),
             extension: fileTypeData.ext,
-            url: '',
+            url: Utils.asset(this.environmentService.get('APP_URL'), 'public/storage', relativeStoragePath, hashName),
             mime: fileTypeData.mime,
-            size: size,
-            sort: ''
+            size: stats.size
         };
     }
 }
