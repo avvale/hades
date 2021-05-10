11c13f200f02cf0919fffadd69706b7d345a5797
carlospalacin <carlos.rodriguez.palacin@gmail.com>
2021-05-10 18:20:21 +0200

    add dataFactory to repository

--- src/@hades/shared/domain/persistence/repository.ts
+++ src/@hades/shared/domain/persistence/repository.ts
@@ -1,5 +1,5 @@
 import { QueryStatement } from '@hades/shared/domain/persistence/sql-statement/sql-statement';
-import { CQMetadata } from '@hades/shared/domain/lib/hades.types';
+import { CQMetadata, ObjectLiteral } from '@hades/shared/domain/lib/hades.types';
 import { ValueObject } from '@hades/shared/domain/value-objects/value-object';
 import { Pagination } from '@hades/shared/domain/lib/pagination';
 
@@ -11,10 +11,10 @@ export interface IRepository<Aggregate>
     paginate(queryStatement: QueryStatement, constraint?: QueryStatement, cQMetadata?: CQMetadata): Promise<Pagination<Aggregate>>;
 
     // create a single record
-    create(item: Aggregate): Promise<void>;
+    create(item: Aggregate, dataFactory?: (aggregate: Aggregate) => ObjectLiteral): Promise<void>;
 
     // create a single or multiple records
-    insert(items: Aggregate[], options: object): Promise<void>;
+    insert(items: Aggregate[], options: object, dataFactory?: (aggregate: Aggregate) => ObjectLiteral): Promise<void>;
 
     // find a single record
     find(queryStatement?: QueryStatement, constraint?: QueryStatement, cQMetadata?: CQMetadata): Promise<Aggregate | null>;
@@ -26,7 +26,7 @@ export interface IRepository<Aggregate>
     get(queryStatement?: QueryStatement, constraint?: QueryStatement, cQMetadata?: CQMetadata): Promise<Aggregate[]>;
 
     // update record
-    update(item: Aggregate, constraint?: QueryStatement, cQMetadata?: CQMetadata): Promise<void>;
+    update(item: Aggregate, constraint?: QueryStatement, cQMetadata?: CQMetadata, dataFactory?: (aggregate: Aggregate) => ObjectLiteral): Promise<void>;
 
     // delete record by id
     deleteById(id: ValueObject<String>, constraint?: QueryStatement, cQMetadata?: CQMetadata): Promise<void>;
